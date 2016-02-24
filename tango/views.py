980bed48f3b3f4dd558cfbd457ca6a658ef70824
arya <raiabhishek04@gmail.com>
2016-02-25 04:03:34 +0530

    Debug the Invalid Login Form

--- tango/views.py
+++ tango/views.py
@@ -195,7 +195,7 @@ def user_login(request):
 				return HttpResponse("Your acccount is disabled")
 
 		else:
-			print "Invalid login details [0], [1]".format[username, password]
+			print "Invalid login details {}, {}".format[username, password]
 			return HttpResponse("Invalid login details supplied.")
 
 	else:
